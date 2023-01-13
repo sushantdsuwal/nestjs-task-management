9fdf76d67bb88e787857652e5f7c86376344500a
Sushant Suwal <sushantdsuwal@gmail.com>
2023-01-13 11:56:33 +0545

    fix AuthModule imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { User } from './user.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [TypeOrmModule.forFeature([User])], //user entity
   providers: [AuthService, UserRepository],
   controllers: [AuthController],
 })
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,23 +1,22 @@
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
-export class UserRepository extends Repository<User> {
-  constructor(
-    @InjectRepository(User)
-    private userRepository: Repository<User>,
-  ) {
-    super(
-      userRepository.target,
-      userRepository.manager,
-      userRepository.queryRunner,
-    );
-  }
-
+@Injectable()
+export class UserRepository {
+  @InjectRepository(User)
+  private userRepository: Repository<User>;
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.userRepository.create({ username, password });
-    await this.save(user);
+    // const user = this.userRepository.create({ username, password });
+    // await this.userRepository.save(user);
+    await this.userRepository
+      .createQueryBuilder()
+      .insert()
+      .into(User)
+      .values({ username, password })
+      .execute();
   }
 }
